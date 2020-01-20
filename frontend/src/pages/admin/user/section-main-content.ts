e25c6905f89f11474802c755f5e82787f8263e09
Bronzato1 <thierry.langie@skynet.be>
2020-01-20 18:58:50 +0100

    Some changes

--- frontend/src/pages/admin/user/section-main-content.ts
+++ frontend/src/pages/admin/user/section-main-content.ts
@@ -2,8 +2,9 @@
 
 import { User } from 'models/user-model';
 import { UserGateway } from './../../../gateways/user-gateway';
-import { autoinject } from 'aurelia-framework';
+import { autoinject, computedFrom, observable } from 'aurelia-framework';
 import { EventAggregator, Subscription } from 'aurelia-event-aggregator';
+import { IFilter, IPager } from '../../../interfaces/filter-interface';
 import 'jquery.magnific-popup.min';
 
 @autoinject()
@@ -17,7 +18,11 @@ export class SectionMainContent {
     userGateway: UserGateway;
     ea: EventAggregator;
     subscription: Subscription;
-    users: User[];
+    totalItems: User[];
+    pagedItems: User[];
+    pager: IPager;
+    @observable()
+    filter: string;
 
     created() {
     }
@@ -33,7 +38,7 @@ export class SectionMainContent {
                 .deleteById(userId)
                 .then(() => {
                     console.log(`User #${userId} deleted`);
-                    var user = this.users.find(x => x.id == userId);
+                    var user = this.pagedItems.find(x => x.id == userId);
                     user.status = 2; // Deleted
                 })
                 .catch((err) => console.log('Error: ' + err));
@@ -45,9 +50,9 @@ export class SectionMainContent {
     async attached() {
 
         const wait = (ms) => new Promise(res => setTimeout(res, ms));
-        await this.userGateway.getAll().then((data) => this.users = data);
+        await this.userGateway.getAll().then((data) => this.totalItems = data);
+        this.setPage(1);
         await wait(100);
-
         this.initializeMagnificPopup();
     }
     initializeMagnificPopup() {
@@ -93,5 +98,85 @@ export class SectionMainContent {
                 return 'main__table-text--red';
                 break;
         }
+    }
+    range(start, end) {
+        return Array.from(new Array(end - start), (x, i) => i + start)
+    }
+    setPage(page) {
+
+        var self = this;
+
+        // get pager object from service
+        this.pager = this.getPager(this.filteredItems.length, page, 10);
+
+        if (page < 1 || this.pager && page > this.pager.totalPages) {
+            this.pagedItems = [];
+            return;
+        }
+
+        // get current page of items
+        this.pagedItems = this.filteredItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+
+    }
+    getPager(totalItems, currentPage, pageSize): IPager {
+
+        // default to first page
+        currentPage = currentPage || 1;
+
+        // calculate total pages
+        var totalPages = Math.ceil(totalItems / pageSize);
+
+        var startPage, endPage;
+        if (totalPages <= 5) {
+            // less than 5 total pages so show all
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            // more than 5 total pages so calculate start and end pages
+            if (currentPage <= 3) {
+                startPage = 1;
+                endPage = 5;
+            } else if (currentPage + 3 >= totalPages) {
+                startPage = totalPages - 4;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 2;
+                endPage = currentPage + 2;
+            }
+        }
+
+        // calculate start and end item indexes
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+        // create an array of pages to ng-repeat in the pager control
+        var pages = this.range(startPage, endPage + 1);
+
+        // return object with all pager properties required by the view
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
+    }
+    filterChanged() {
+        this.setPage(1);
+    }
+    get filteredItems() {
+
+        if (!this.totalItems) return [];
+
+        var filteredItems: User[] = this.totalItems.slice();
+
+        if (this.filter)
+            filteredItems = filteredItems.filter(x => x.userName.toLocaleLowerCase().includes(this.filter.toLocaleLowerCase()));
+
+        return filteredItems;
     }
 }
